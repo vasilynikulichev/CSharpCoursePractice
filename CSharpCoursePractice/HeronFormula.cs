cc58fd72a0a63f7e7d8f0152a2d9a1b7aaa28d73
vasilijnikulicev <vasilynikulichev@gmail.com>
2021-05-15 16:26:02 +0300

    add project and first task

---
+++ CSharpCoursePractice/HeronFormula.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpCoursePractice
+{
+    public class HeronFormula
+    {
+        public HeronFormula()
+        {
+            Console.WriteLine("ab = ");
+            double ab = int.Parse(Console.ReadLine());
+            Console.WriteLine("bc = ");
+            double bc = int.Parse(Console.ReadLine());
+            Console.WriteLine("ca = ");
+            double ca = int.Parse(Console.ReadLine());
+
+            double p = (ab + bc + ca) / 2;
+            double s = Math.Sqrt(p * (p - ab) * (p - bc) * (p - ca));
+            Console.WriteLine($"ab = {ab}; bc = {bc}; ca = {ca};");
+            Console.WriteLine("S = " + s);
+        }
+    }
+}
---
+++ CSharpCoursePractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpCoursePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HeronFormula heronFormula = new HeronFormula();
+        }
+    }
+}
